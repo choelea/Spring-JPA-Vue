a7223cd8e83841ff2caff1c0a581fefec604c373
bighb <2740916018@qq.com>
2025-04-29 22:11:27 +0800

    修改脚本

--- admin-ui/deploy-zip.sh
+++ admin-ui/deploy-zip.sh
@@ -4,9 +4,9 @@
 pnpm run build
 
 # 第二步：检查并删除现有的 zip 包
-if [ -f "admin.zip" ]; then
-    rm admin.zip
+if [ -f "dist.zip" ]; then
+    rm dist.zip
 fi
 
 # 第三步：将 dist 目录打包成 zip 包
-zip -r admin.zip dist/
+zip -r dist.zip dist/
