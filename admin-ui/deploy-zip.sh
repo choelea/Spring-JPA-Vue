fb823fd2032c0a8540f9c316c9627f914f18563d
Joe Li <joe.lea@foxmail.com>
2025-04-29 22:24:07 +0800

    update

--- admin-ui/deploy-zip.sh
+++ admin-ui/deploy-zip.sh
@@ -3,10 +3,7 @@
 # 第一步：运行生产环境构建
 pnpm run build
 
-# 第二步：检查并删除现有的 zip 包
-if [ -f "admin.zip" ]; then
-    rm admin.zip
-fi
 
-# 第三步：将 dist 目录打包成 zip 包
-zip -r admin.zip dist/
+# 第二步：将 dist 目录下的内容复制到 ../src/main/resources/static/admin/ 目录下
+rm -rf ../src/main/resources/static/admin/*
+cp -r dist/* ../src/main/resources/static/admin/
